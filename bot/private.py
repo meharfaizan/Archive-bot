fd2c84fa42b5eee9f5ec761b1dec277370e406e9
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2021-07-17 22:04:55 +0530

    Update private.py

--- bot/private.py
+++ bot/private.py
@@ -1,14 +1,10 @@
 from pyrogram import Client, filters, types
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
-from pyrogram.errors import PeerIdInvalid, ChannelInvalid, FloodWait
-from pyrogram.emoji import *
 
 from zipfile import ZipFile
 from os import remove, rmdir, mkdir
 
 from utils import zip_work, dir_work, up_progress, list_dir, Msg, db_session, User, commit
 from sample_config import Config
-
 
 @Client.on_message(filters.command("start"))
 def start(_, msg: types.Message):
@@ -48,27 +44,27 @@ def start_zip(_, msg: types.Message):
 def enter_files(_, msg: types.Message):
     """ download files """
     uid = msg.from_user.id
- 
+
     with db_session:
         usr = User.get(uid=uid)
         if usr.status == 1:  # check if user-status is "INSERT"
 
             type = msg.document or msg.video or msg.photo or msg.audio
 
-            if type.file_size > 2000000000:
+            if type.file_size > 2097152000:
                 msg.reply(Msg.too_big)
-            elif len(list_dir(uid)) > 501:
+            elif len(list_dir(uid)) > 500:
                 msg.reply(Msg.too_much)
             else:
                 downsts = msg.reply(Msg.downloading, True)  # send status-download message
                 msg.download(dir_work(uid))
-                
+
                 downsts.delete()  # delete status-download message
 
         else:
             msg.reply(Msg.send_zip)  # if user-status is not "INSERT"
 
-    
+
 @Client.on_message(filters.command("stopzip"))
 def stop_zip(_, msg: types.Message):
     """ exit from insert mode and send the archive """
@@ -107,7 +103,7 @@ def stop_zip(_, msg: types.Message):
                            progress_args=(stsmsg,))
     except ValueError as e:
         msg.reply(Msg.unknow_error.format(str(e)))
-            
+
     stsmsg.delete()  # delete the status-msg
     remove(zip_path)  # delete the zip-archive
     rmdir(dir_work(uid))  # delete the static-folder
